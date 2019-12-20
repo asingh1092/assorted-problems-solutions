f801526b5bdcbb8ae3595a7dd7707d961ae00ba1
Amrit Singh <asingh1092@gmail.com>
2019-12-20 17:18:07 -0500

    completed solutions
    Signed-off-by: Amrit Singh <asingh1092@gmail.com>

--- src/com/singh/EPI/Arrays/IntAsArrayIncrement.java
+++ src/com/singh/EPI/Arrays/IntAsArrayIncrement.java
@@ -1,10 +1,11 @@
 package com.singh.EPI.Arrays;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class IntAsArrayIncrement {
 
-	public static List<Integer> incremeentArray(List<Integer> A) {
+	public static List<Integer> incrementArray1(List<Integer> A) {
 		int carry = 0;
 		for (int i = A.size() - 1; i == 0; i--) {
 			if (carry > 1) {
@@ -19,5 +20,38 @@ public class IntAsArrayIncrement {
 
 		return A;
 	}
+	
+	public static List<Integer> incrementArray(List<Integer> A) {
+		int n = A.size() - 1; // 
+		A.set(n, A.get(n) + 1);
+		
+		for (int i = n; i > 0 && A.get(i) == 10; i--) {
+			A.set(i, 0);
+			A.set(i - 1, A.get(i - 1) + 1);
+		}
+		
+		if (A.get(0) == 10) {
+			A.set(0,  1);
+			A.add(0);
+		}
 
+		return A;
+	}
+	
+	public static void main(String args[]) {
+		List<Integer> test1 = new ArrayList<>();
+		List<Integer> test2 = new ArrayList<>();
+		List<Integer> test3 = new ArrayList<>();
+		List<Integer> test4 = new ArrayList<>();
+		
+		test1.add(9);
+		test2.add(1); test2.add(9); test2.add(9);
+		test3.add(1); test3.add(2); test3.add(9);
+		test4.add(1); test4.add(2); test4.add(8);
+		
+		System.out.println(incrementArray(test1));
+		System.out.println(incrementArray(test2));
+		System.out.println(incrementArray(test3));
+		System.out.println(incrementArray(test4));
+	}
 }
--- src/com/singh/EPI/Arrays/IntAsArrayMultiply.java
+++ src/com/singh/EPI/Arrays/IntAsArrayMultiply.java
@@ -1,34 +1,64 @@
 package com.singh.EPI.Arrays;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class IntAsArrayMultiply {
 
 	public static List<Integer> intAsArrayMultiply(List<Integer> A, List<Integer> B) {
-		List<Integer> result = new ArrayList<>();
-		int longerOfTheTwoSize;
-		if (B.size() < A.size()) {
-			longerOfTheTwoSize = A.size();
-		} else {
-			longerOfTheTwoSize = B.size();
-		} 
-		int carry;
-		for (int i = longerOfTheTwoSize - 1; i >= 0 ; i++) {
-			result.add((int) Math.floor(A.get(i) * B.get(i)));
-			if (-1 < A.get(i) * B.get(i) / 10 && A.get(i) * B.get(i) / 10  < 1) {
-				result.add((int) Math.floor(A.get(i) * B.get(i)) / 10);
-				carry = (int) Math.floorDiv(A.get(i), B.get(i));
-			} else {
-				carry = (int) Math.floorDiv(A.get(i), B.get(i));
+		// Deal with sign -> deal with it at the end
+		int sign = 1;
+		if (A.get(0) < 0 || B.get(0) < 0) {
+			sign = -1;
+		}
+		A.set(0, Math.abs(A.get(0)));
+		B.set(0, Math.abs(B.get(0)));
+		
+		// Create new list of n+m size
+		List<Integer> result = new ArrayList<>(Collections.nCopies(A.size() + B.size(), 0));
+
+		for (int i = A.size() - 1; i >= 0; --i) {
+			for (int j = B.size() - 1; j >= 0; --j) {
+				result.set(i + j + 1, result.get(i + j + 1) + A.get(i) * B.get(j));
+				result.set(i + j, result.get(i + j) + result.get(i + j + 1) / 10);
+				result.set(i + j + 1, result.get(i + j + 1) % 10);
 			}
 		}
+
+		// Deal with leading zeros
+		int leadingZero = 0;
+		while (leadingZero < result.size() && result.get(leadingZero) == 0) {
+			leadingZero++;
+		}
+
+		result = result.subList(leadingZero, result.size());
+		
+		// Deal with empty list
+		if (result.isEmpty()) {
+			return Collections.emptyList();
+		}
+		
+		// Add sign to first digit in result 
+		result.set(0, result.get(0) * sign);
 		return result;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Integer> listA = new ArrayList<>();
+		List<Integer> listB = new ArrayList<>();
+		Random rand = new Random();
 
+		for (int i = 0; i < 6; i++) {
+			listA.add(rand.nextInt(10));
+		}
+		
+		for (int i = 0; i < 3; i++) {
+			listB.add(rand.nextInt(10));
+		}
+
+		System.out.println(intAsArrayMultiply(listA, listB));
 	}
 
 }

a316e9830969caf05f177d62192520fc10375d14
Amrit Singh <asingh1092@gmail.com>
2019-12-26 17:16:29 -0500

    QueueFromStacks solution complete

--- src/com/singh/EPI/Queue/QueueFromStacks.java
+++ src/com/singh/EPI/Queue/QueueFromStacks.java
@@ -4,18 +4,28 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class QueueFromStacks {
-	
+
 	public static class Queue {
 		private Deque<Integer> enqueue = new ArrayDeque<>();
 		private Deque<Integer> dequeue = new ArrayDeque<>();
-		
+
 		// enqueue
-		public void enqueueStack() {
-			
+		public void enqueueStack(Integer n) {
+			enqueue.addFirst(n);
 		}
+
 		// deqeue
-		
-		
+		public Integer dequeueStack() {
+			if (!dequeue.isEmpty()) {
+				return dequeue.removeFirst();
+			} else {
+				while (!enqueue.isEmpty()) {
+					dequeue.addFirst(enqueue.removeFirst());
+				}
+				return dequeue.removeFirst();
+			}
+		}
+
 	}
 
 	public static void main(String[] args) {

5e38cb0d088ca345d150c9f5f3b88351e694c8fa
Amrit Singh <asingh1092@gmail.com>
2019-12-28 22:18:28 -0500

    setting up heaps and binary search algorithms

---
+++ src/com/singh/EPI/Heaps/TopK.java
@@ -0,0 +1,12 @@
+package com.singh.EPI.Heaps;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class TopK {
+	
+	public static List<String> topk(int k, Iterator<String> iter) {
+		
+	}
+
+}
---
+++ src/com/singh/EPI/Searching/RealSquareRoot.java
@@ -0,0 +1,5 @@
+package com.singh.EPI.Searching;
+
+public class RealSquareRoot {
+
+}
